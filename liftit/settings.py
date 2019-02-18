348aba1ab839f568330db33f99be41d04bfec750
efajardol <eduardo.fajardo.lopez_@gmail.com>
2019-02-17 19:38:36 -0500

    remove static db config

--- liftit/settings.py
+++ liftit/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'liftit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'liftit_store_drf',
-        'USER': 'liftit',
-        'PASSWORD': 'lifit123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'liftit_store_drf',
+#        'USER': 'liftit',
+#        'PASSWORD': 'lifit123',
+#        'HOST': '127.0.0.1',
+# 'PORT': '5432'
+#    }
+# }
 
 #DATABASES = {
 #    'default': {
