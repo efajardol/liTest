fa2681b0291352ac773c07155b452be1ba9287ec
efajardol <eduardo.fajardo.lopez_@gmail.com>
2019-02-18 08:05:52 -0500

    typo

--- liftit/settings.py
+++ liftit/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DriversVehicles',
-    'rest_framework'
+    'rest_framework',
+    'DriversVehicles'
 ]
 
 MIDDLEWARE = [
